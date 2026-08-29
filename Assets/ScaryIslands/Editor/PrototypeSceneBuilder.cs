bdfaba3067db26cd7d1f939097f2c2093186130c
Adam Pluguez <70963681+adaPlu@users.noreply.github.com>
2026-08-28 21:30:24 -0700

    Place pet shop in prototype scene

--- Assets/ScaryIslands/Editor/PrototypeSceneBuilder.cs
+++ Assets/ScaryIslands/Editor/PrototypeSceneBuilder.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using ScaryIslands.Game;
+using ScaryIslands.Pets;
 using ScaryIslands.World;
 
 namespace ScaryIslands.Editor
@@ -14,19 +15,49 @@ namespace ScaryIslands.Editor
         {
             if (!AssetDatabase.IsValidFolder("Assets/ScaryIslands/Scenes"))
                 AssetDatabase.CreateFolder("Assets/ScaryIslands", "Scenes");
+
             var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
             new GameObject("Run State").AddComponent<RunState>();
-            var light = new GameObject("Moonlight").AddComponent<Light>(); light.type = LightType.Directional; light.intensity = .35f; light.color = new Color(.55f,.7f,.66f);
-            for (int x=-4;x<=4;x++) for(int z=-4;z<=4;z++)
+
+            var light = new GameObject("Moonlight").AddComponent<Light>();
+            light.type = LightType.Directional;
+            light.intensity = .35f;
+            light.color = new Color(.55f, .7f, .66f);
+
+            for (int x = -4; x <= 4; x++)
+            for (int z = -4; z <= 4; z++)
             {
-                var tile=GameObject.CreatePrimitive(PrimitiveType.Cube); tile.name="Island Ground"; tile.transform.position=new Vector3(x*4,-.6f,z*4); tile.transform.localScale=new Vector3(4,.8f,4);
+                var tile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                tile.name = "Island Ground";
+                tile.transform.position = new Vector3(x * 4, -.6f, z * 4);
+                tile.transform.localScale = new Vector3(4, .8f, 4);
             }
-            var tide=GameObject.CreatePrimitive(PrimitiveType.Cube); tide.name="Black Tide"; tide.transform.localScale=new Vector3(80,.25f,80); tide.AddComponent<BlackTide>();
-            var spawn=new GameObject("XR Origin Spawn"); spawn.transform.position=new Vector3(0,1, -12);
-            var key=GameObject.CreatePrimitive(PrimitiveType.Cylinder); key.name="Chapel Key"; key.transform.position=new Vector3(-9,1,3); key.AddComponent<ObjectivePickup>();
-            var bell=GameObject.CreatePrimitive(PrimitiveType.Sphere); bell.name="Salt Bell"; bell.transform.position=new Vector3(10,1,8); bell.AddComponent<ObjectivePickup>();
-            EditorSceneManager.SaveScene(scene,"Assets/ScaryIslands/Scenes/WidowsShore.unity");
-            Debug.Log("Scary Islands prototype scene created. Add an XR Origin prefab from XR Interaction Toolkit samples at XR Origin Spawn.");
+
+            var tide = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            tide.name = "Black Tide";
+            tide.transform.localScale = new Vector3(80, .25f, 80);
+            tide.AddComponent<BlackTide>();
+
+            var spawn = new GameObject("XR Origin Spawn");
+            spawn.transform.position = new Vector3(0, 1, -12);
+
+            var key = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            key.name = "Chapel Key";
+            key.transform.position = new Vector3(-9, 1, 3);
+            key.AddComponent<ObjectivePickup>();
+
+            var bell = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            bell.name = "Salt Bell";
+            bell.transform.position = new Vector3(10, 1, 8);
+            bell.AddComponent<ObjectivePickup>();
+
+            var petShop = new GameObject("Pet Shop");
+            petShop.transform.position = new Vector3(5, 0, -8);
+            petShop.transform.rotation = Quaternion.Euler(0, 205, 0);
+            petShop.AddComponent<PetShopTerminal>();
+
+            EditorSceneManager.SaveScene(scene, "Assets/ScaryIslands/Scenes/WidowsShore.unity");
+            Debug.Log("Scary Islands prototype scene created. Add an XR Origin prefab at XR Origin Spawn. The Pet Shop is placed near the starting area and uses Dots.");
         }
     }
 }
